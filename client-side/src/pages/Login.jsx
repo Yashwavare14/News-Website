c234c9da0e1295cdebd5c14104ff87c686da5a0a
Yashwavare14 <160131124+Yashwavare14@users.noreply.github.com>
2024-11-29 20:23:13 +0530

    Update Login.jsx

--- client-side/src/pages/Login.jsx
+++ client-side/src/pages/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
     }
     try {
       const response = await axios.post(
-        "https://news-website-server.onrender.com",
+        "https://news-website-server.onrender.com/auth/login",
         loginInfo,
         {
           headers: {
