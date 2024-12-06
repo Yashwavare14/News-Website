f6cc3efbb216c018762e5f0ef03e2b56d23c2510
Yashwavare14 <160131124+Yashwavare14@users.noreply.github.com>
2024-12-06 22:04:20 +0530

    Update NewsBoard.jsx

--- client-side/src/components/layout/NewsBoard.jsx
+++ client-side/src/components/layout/NewsBoard.jsx
@@ -3,12 +3,16 @@ import NewsCard from "./NewsCard";
 import { NewsContext } from "../../context/Newscontext";
 
 export default function NewsBoard() {
-  const { articles } = useContext(NewsContext);
+  const { articles, loading } = useContext(NewsContext);
 
   return (
     <div className="flex flex-col bg-gray-100 pt-1 md:pt-6">
       <div className="flex flex-wrap justify-center ">
-        {articles?.map((news, index) => {
+        { ( loading ? ( // Conditional rendering
+        <div className="flex justify-center items-center h-screen text-xl font-semibold">
+          Loading...
+        </div>
+      ) :  articles?.map((news, index) => {
           return (
             <NewsCard
               key={index}
@@ -18,7 +22,7 @@ export default function NewsBoard() {
               url={news.url}
             />
           );
-        })}
+        }) )}
       </div>
     </div>
   );
